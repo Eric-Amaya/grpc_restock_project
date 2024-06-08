660c95619d7440a9edea8ab621cc717a80c1c086
Eric-Amaya <eric.amaya@alumnos.ucn.cl>
2024-06-08 19:40:38 -0400

    dockerfile config

--- grpc-go-proto-product-ms-main/pkg/config/config.go
+++ grpc-go-proto-product-ms-main/pkg/config/config.go
@@ -1,7 +1,9 @@
 package config
 
-import "github.com/spf13/viper"
-
+import (
+	"github.com/spf13/viper"
+	"log"
+)
 type Config struct {
 	Port  string `mapstructure:"PORT"`
 	DBUrl string `mapstructure:"DB_URL"`
